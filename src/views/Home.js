6a5193d6e7a0c4af5e0dc81c1375ca5c6b2befc2
vishal <bhalothiavishal@gmail.com>
2022-06-17 19:02:17 +0530

    map and charts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+const GoogleMapReact = React.lazy(() => import("../views/Charts/GoogleMap"));
 const Header = React.lazy(() => import('../DefaultLayout/Header'));
 const Nav = React.lazy(() => import('../DefaultLayout/Nav'));
 const data = require("../Datasheet/data.json");
@@ -10,6 +11,13 @@ function Home() {
     const [mapData, setMapData] = useState();
     const [currentKegId, setCurrentKegId] = useState("");
 
+    const options = {
+        center: {
+            lat: mapData?.latitude || 51.4835741, lng: mapData?.longitude || -0.1252037
+        },
+        zoom: 15,
+    }
+
     const getkegIdList = () => {
         setKegIds(allData.map((v) => v.kegtrackerId));
     }
@@ -19,6 +27,14 @@ function Home() {
         setCurrentKegId(kegid);
         setMapData(allData.find((val) => val.kegtrackerId == kegid))
     }
+    const addMarkers = (map, links) => {
+        const marker = new window.google.maps.Marker({
+            map,
+            position: { lat: mapData?.latitude || 51.4835741, lng: mapData?.longitude || -0.1252037 },
+            label: mapData.Product,
+            title: `${mapData.Product} ( ${mapData.placename})`,
+        })
+    }
 
     useEffect(() => {
         getkegIdList();
@@ -26,7 +42,7 @@ function Home() {
 
     return (
         <div className="outerLayout">
-            <Header  title="Home Page"/>
+            <Header title="Home Page" />
             <Nav />
             <aside>
                 <div>
@@ -44,6 +60,10 @@ function Home() {
                     <div> Volume : {mapData.volume} %</div>
                     <div> Bettery : {mapData.battery} %</div>
 
+                </div>}
+
+                {mapData && <div className="googlemap">
+                    <GoogleMapReact options={options} onMount={addMarkers} onMountProps={{ coords: { lat: mapData?.latitude || 51.4835741, lng: mapData?.longitude || -0.1252037 } }} />
                 </div>}
             </aside>
         </div>
--- src/views/Retailer.js
+++ src/views/Retailer.js
@@ -8,42 +8,61 @@ const data = require("../Datasheet/data.json");
 function Retailer() {
 
     const [allData, setAllData] = useState(data);
-    const [types, setTypes] = useState([]);
-    const [typeList, setTypeList] = useState([]);
+    const [chartProduct, setChartProduct] = useState([]);
+    const [productsName, setProductsName] = useState([]);
+    const [productsCount, setProductsCount] = useState([]);
+    const [colors, setColors] = useState([]);
+    const [total, setTotal] = useState(0);
 
     const getTypesList = () => {
-        setTypes(allData.filter((val) => {
-            if (val.location === 'Retailer' && (val.volume > 90 && val.volume <= 100)) {
-                if (!typeList.includes(val.Type)) {
-                    let arr = [...typeList];
-                    arr.push(val.Type);
-                    setTypeList(arr)
+        let filteredProduct = allData.filter((val) => (val.location === 'Retailer' && (val.volume > 90 && val.volume <= 100)))
+        let products = []
+        filteredProduct.forEach((value) => {
+            let key = value.Product;
+            if (products[key]) {
+                products[key].count = products[key].count + 1;
+            } else {
+                products[key] = {
+                    count: 1,
+                    name: key,
+                    color: '#' + Math.floor(Math.random() * 16777215).toString(16)
                 }
-                return val;
             }
-        })
-        )
+        })        
+        setChartProduct(Object.values(products));
+        setProductsName(Object.keys(products));
+        setProductsCount(Object.values(products).map(v => v.count));
+        setTotal(Object.values(products).map(v => v.count).reduce((a, b) => a + b, 0));
+        setColors(Object.values(products).map(v => v.color));
     }
-
 
     useEffect(() => {
         getTypesList();
     }, [])
-
-    useEffect(() => {
-        console.log('-------->', typeList)
-    }, [typeList])
 
     return (
         <div className="outerLayout">
             <Header title="Retailer Full Product" />
             <Nav />
             <aside>
-                <div className="retailer-chart">
-                    <RetailerChart />
+                <div className="retailer-details">
+                    {
+                        chartProduct.map((item, i) => {
+                            return <div className="list" key={i}>
+                                <span style={{ backgroundColor: item.color, height: '15px', width: '15px', float: 'left', clear: 'both' }}></span>
+                                <span className="productName"> {item.name} </span> <span className="count"> {item.count}</span>
+                            </div>
+                        })
+                    }
                 </div>
-            </aside>
-        </div>
+                <div className="retailer-chart">
+                    <RetailerChart productsName={productsName} productsCount={productsCount} colors={colors} />
+                </div>
+                <div className="retailer-details">
+                    <h2>  Total : {total} </h2>
+                </div>
+            </aside >
+        </div >
     )
 }
 
